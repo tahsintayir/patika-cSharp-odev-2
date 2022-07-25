879573ce795c57f83ac41322e58426902f005116
tahsintayir <tahsintayir@hotmail.com>
2022-07-25 20:57:30 +0300

    last-commit

--- patik-cSharp-odev-2/patik-cSharp-odev-2/Program.cs
+++ patik-cSharp-odev-2/patik-cSharp-odev-2/Program.cs
@@ -10,6 +10,58 @@ namespace patik_cSharp_odev_2
     {
         static void Main(string[] args)
         {
+            int x = 2;
+            int y = 4;
+            y = y + 2;
+            Console.WriteLine(y);
+
+            y /= 2;
+            Console.WriteLine(y);
+            
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfoct");
+
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great");
+
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine");
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1++;
+            Console.WriteLine(sonuc1);
+
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);  
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
